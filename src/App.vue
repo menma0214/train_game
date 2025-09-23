d301057b3dcce4cde79c4fd668173570f65d0fe9
menma0214 <116738944+menma0214@users.noreply.github.com>
2025-09-23 22:32:28 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,3 @@
+<template><h1>電車ゲーム</h1></template>
+<script setup lang="ts"></script>
+<style scoped></style>
---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    host: true, //Dockerから外へ公開
+    port: 5173,
+    // strictPort: true,
+    watch: {usePolling: true} //コンテナ越しの監視を安定化
+    // hmr: { clientPort: 5173 } //HMRが不安定な場合のみ有効化
+  }
+})
